faeaf10e5be92e84715af1e9fa3cae0625ca6223
roy1210 <rswkure@gmail.com>
2021-12-01 19:26:38 +0800

    fix: readme

--- src/modules/apr-convert/test/index.spec.ts
+++ src/modules/apr-convert/test/index.spec.ts
@@ -1,8 +1,12 @@
-import { aprToApy } from './../index';
+import { aprToApy, apyToApr } from './../index';
 
-describe('apr-convert test', () => {
+describe('convert test', () => {
   it('convert apr to apy', () => {
     expect(aprToApy(9.3)).toEqual(9.74617334187522);
     expect(aprToApy(15.1)).toEqual(16.299665194576196);
+  });
+  it('convert apy to apr', () => {
+    expect(apyToApr(12.3)).toEqual(11.600367887465119);
+    expect(apyToApr(24.7)).toEqual(22.074067738441073);
   });
 });
